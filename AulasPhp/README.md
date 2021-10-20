8d4e77ca124fa3d8840bccb32093bf3b3d57af9b
VNRodriguez13 <65317241+VNRodriguez13@users.noreply.github.com>
2021-10-20 20:50:53 -0300

    Add files via upload

---
+++ AulasPhp/README.md
@@ -0,0 +1,7 @@
+## Programação Back-End
+* **Dia:** Quarta-Feira 
+* **Período:** Noite
+
+## Prof. Hebert Bratefixe Alquimim
+
+e-mail: [hebert@uni9.pro.br](mailto:hebert@uni9.pro.br)
---
+++ AulasPhp/aula5/aula5_091.php
@@ -0,0 +1,13 @@
+<?php
+$i=7; // inicialização do nosso contador;
+do {
+  echo $i . "<br> ";
+  $i+=2; // contador
+} while ($i < 5);
+echo "Valor da variável i = $i;"
+/*
+Saída:
+7
+Valor da variável i = 9
+*/
+?>
